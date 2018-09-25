cc8cff8f536bee401f3391c28e67757ead4b1d9d
otadewosjan <oriana.an@outlook.com>
2018-09-25 11:00:06 +0200

    getting all posts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 app.use(express.static('files'));
@@ -7,3 +8,14 @@ app.use(express.static('files'));
 app.listen(port);
 
 console.log("server started on port: " + port);
+
+app.get('/posts', allPosts);
+
+function allPosts(request, response) {
+    if (fs.existsSync('posts.json')) {
+        let items = fs.readFileSync('posts.json');
+        response.send(JSON.parse(items));
+    } else {
+        response.send("no posts");
+    }
+}
